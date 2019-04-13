8855783f111e2753c4463ffb4f12e8cc76bdc770
Lucas Blanco <lucasblanco3107@gmail.com>
2019-04-13 14:59:30 -0300

    Cambiamos uso de Puertos y modificamos loop de retardo

--- TP1/Lucas/TP1.asm
+++ TP1/Lucas/TP1.asm
@@ -17,11 +17,11 @@
  goto 0x05
  ORG 0X05
  
- BANKSEL PORTA
- CLRF PORTA
- BANKSEL TRISA
+ BANKSEL PORTC
+ CLRF PORTC
+ BANKSEL TRISC
  MOVLW B'00000000'
- MOVWF TRISA
+ MOVWF TRISC
  
  BANKSEL PORTB
  CLRF PORTB
@@ -36,12 +36,15 @@ CBLOCK 0X22
  CONT2
  CONT3
  ENDC
+ 
 INICIO
   BANKSEL PIR1
- ; MOVF PORTB
+  MOVF PORTB,0
+ 
+  ;Identifica correctamente los 2 numeros:
   CLRF AX ;Setea todos los bits en cero
   CLRF BX ;Setea todos los bits en cero
-  MOVLW B'11100001' ;Setea el valor en W
+  ;MOVLW B'11100001' ;Setea el valor en W
   MOVWF BX ;Mueve el valor de W a B
   ANDLW 0X0F ;Realiza un AND logico de k con W
   MOVWF AX ;Mueve el resultado a AX
@@ -49,21 +52,23 @@ INICIO
   ANDLW 0XF0 ; Realiza un AND logico de k con W
   MOVWF BX ;Mueve el valor de W a BX
   SWAPF BX,1 ;Swapea los bits 7a4 con los de 3a0 y los guarda en BX
+  
+  ;Realiza la suma:
   MOVF AX,0 ;Mueve el valor de AX a W
   ADDWF BX,1 ;Suma el valor que contiene W con el de BX y lo guarda en BX
   BTFSC BX,4 ;Se fija si el bit de la posicion 4 es 1 o 0 (osea si hay carry o no), si es 1 la siguiente instruccion se ejecuta
-  GOTO TITILO ;Va a TITILO
-  MOVF BX,0 ;Como no se ejecuto, mueve el valor de BX a W
-  MOVWF PORTA ;Mueve el valor de W a PORTA
+  GOTO LEDON ;Va a TITILO
+  MOVF BX,0 ;Como no se ejecuto la anterior, mueve el valor de BX a W
+  MOVWF PORTC ;Mueve el valor de W a PORTA
   GOTO INICIO ;Vuelve a INICIO
 
-TITILO ;Como hay carry tiene que titilar otro LED
-  BSF PORTA,4 ;Setea en 1 el bit de la posicion 4 de PORTA
+LEDON ;Como hay carry tiene que titilar otro LED
+  BSF PORTC,4 ;Setea en 1 el bit de la posicion 4 de PORTA
 LAZO  
-  MOVLW 0X06 ;Mueve 00000110 (6) a W
+  MOVLW 0X08 ;Mueve 00000110 (6) a W
   MOVWF CONT3 ;Mueve el valor de W a CONT3
 LAZO3
-  MOVLW 0XD7 ;Mueve 11011001 (217) a W
+  MOVLW 0XFF ;Mueve 11011001 (217) a W
   MOVWF CONT2 ; Mueve el valor de W a CONT2
 LAZO2
   MOVLW 0XFF ;Mueve 11111111 (255) a W
@@ -73,16 +78,35 @@ LAZO1
   GOTO LAZO1 ;Ciclo de 64 clocks
   DECFSZ CONT2,1 ;Se decrementa el valor de CONT2 y se guarda en CONT2, hasta que sea 0
   GOTO LAZO2 ;Ciclo de -- clocks
-  DECFSZ CONT3,1 ;Se decrementa el valor de CONT3 y se guarda en CONT3, hasta que sea 0
+  DECFSZ CONT3,1 ;Se decrementa el valor de CONT3 y se guarda en CONT3, hasta que el resultado sea 0, cuando esto ocurra salta la prox instruccion
   GOTO LAZO3 ;Ciclo de 7 clocks
+
+LEDOFF ;Como hay carry tiene que titilar otro LED
+  BCF PORTC,4 ;Setea en 1 el bit de la posicion 4 de PORTA
+LAZOF  
+  MOVLW 0X08 ;Mueve 00000110 (6) a W
+  MOVWF CONT3 ;Mueve el valor de W a CONT3
+LAZOF3
+  MOVLW 0XFF ;Mueve 11011001 (217) a W
+  MOVWF CONT2 ; Mueve el valor de W a CONT2
+LAZOF2
+  MOVLW 0XFF ;Mueve 11111111 (255) a W
+  MOVWF CONT1 ;Mueve el valor de W a CONT1
+LAZOF1
+  DECFSZ CONT1,1 ;Se decrementa el valor de CONT1 y se guarda en CONT1. Si el resultado es 0 Sale y un NOP es ejecutado
+  GOTO LAZOF1 ;Ciclo de 64 clocks
+  DECFSZ CONT2,1 ;Se decrementa el valor de CONT2 y se guarda en CONT2, hasta que sea 0
+  GOTO LAZOF2 ;Ciclo de -- clocks
+  DECFSZ CONT3,1 ;Se decrementa el valor de CONT3 y se guarda en CONT3, hasta que el resultado sea 0, cuando esto ocurra salta la prox instruccion
+  GOTO LAZOF3 ;Ciclo de 7 clocks
   
-  BTFSS PORTA,4   ;SALTO SI ESTA PRENDIDO - Si el bit 4 es cero se ejecuta sino salta
-  BSF PORTA,4 ;Setea un 1 en bit 4
+  ;BTFSS PORTA,4   ;SALTO SI ESTA PRENDIDO - Si el bit 4 es cero se ejecuta sino salta
+  ;BSF PORTA,4 ;Setea un 1 en bit 4
   
-  BTFSC PORTA,4   ;SALTO SI ESTA APAGADO - Si el bit 4 es uno se ejecuta sino salta
-  BCF PORTA,4 ;Setea un 0 en bit 4
+  ;BTFSC PORTA,4   ;SALTO SI ESTA APAGADO - Si el bit 4 es uno se ejecuta sino salta
+  ;BCF PORTA,4 ;Setea un 0 en bit 4
   
-  GOTO LAZO ;Vuelve a LAZO
+  GOTO LEDON ;Vuelve a LAZO
  
   GOTO $
   END
